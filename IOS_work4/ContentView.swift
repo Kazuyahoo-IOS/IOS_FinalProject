a03fdff83c7666ba038999f733012f41490af266
kazuyahooo <46344542+kazuyahooo@users.noreply.github.com>
2020-05-17 12:36:08 +0800

    Initial Commit

---
+++ IOS_work4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS_work4
+//
+//  Created by 王瑋 on 2020/5/17.
+//  Copyright © 2020 王瑋. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
